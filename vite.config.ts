9953fa1cd2d8e2201ca7b8865ea6d99e10534b7b
willerswift <116701315+willerswift@users.noreply.github.com>
2026-04-17 13:49:17 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kokoEduWebsite/', // Đường dẫn tương ứng với tên repository trên GitHub
+  base: './', // Sử dụng đường dẫn tương đối để tránh lỗi 404 trên GitHub Pages
 })
